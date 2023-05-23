e62b091e5c3b32b0ea64e044a99c2f2753c621e0
Tonmoy Ahmed Jitu <tonmoyj@byrons.com.au>
2023-05-23 17:07:26 +1000

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-	const [active, setActive] = useState('')
+	const [active, setActive] = useState('');
+	const [toggle, setToggle] = useState(false);
 
 	return (
 		<nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -18,18 +19,38 @@ const Navbar = () => {
 						window.scrollTo(0,0);
 					}}>
 					<img src={logo} alt={logo} className='w-15 h-15 object-contain'/>
-					<p className='text-red-700 text-[52px] font-bold cursor-pointer'>Tonmoy Jitu</p>
+					<p className='text-red-700 text-[52px] font-bold cursor-pointer flex'>Tonmoy Jitu</p>
 				</Link>
 				<ul className='list-none hidden sm:flex flex-row gap-10'>
-					{navLinks.map((link) => (
-						<li key={link.id} className={`${
-							active === link.title ? 'text-white' : 'text-secondary'
-						} hover: text-white text-[18px] font-medium curor-pointer
-						`}>
-							<a href={`#${link.id}`}>{link.title}</a>
-						</li>
-					))}
-				</ul>
+          			{navLinks.map((nav) => (
+            			<li key={nav.id} className={`${
+                			active === nav.title ? "text-white" : "text-secondary"
+              			} hover:text-white text-[18px] font-medium cursor-pointer`}
+              			   onClick={() => setActive(nav.title)}
+            			>
+              				<a href={`#${nav.id}`}>{nav.title}</a>
+            			</li>
+          			))}
+        		</ul>
+				<div className={`${!toggle ? "hidden" : "flex"} 
+					p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+					<ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+						{navLinks.map((nav) => (
+							<li
+							key={nav.id}
+							className={`font-poppins font-medium cursor-pointer text-[16px] ${
+								active === nav.title ? "text-white" : "text-secondary"
+							}`}
+							onClick={() => {
+								setToggle(!toggle);
+								setActive(nav.title);
+							}}
+							>
+							<a href={`#${nav.id}`}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	)
