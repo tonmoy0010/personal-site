9a72f504e3a1ffb2db5b1d60220c6bfa5585b1fd
Tonmoy Ahmed Jitu <tonmoyj@byrons.com.au>
2023-05-23 14:15:37 +1000

    bug fix - page did not load

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import { BrowserRouter } from 'react-router-dom';
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StartsCanvas } from "./components";
 
 const App = () => {
   return (
-	<BrowserRouter>
-		<div className="relative z-0 bg-primary">
-			<div className="bg-hero-patern bg-cover bg-no-repeat bg-center">
-				<Navbar />
-				<Hero />
-			</div>
-			<About />
-			<Experience />
-			<Tech />
-			<Works />
-			<Feedbacks />
-			<div className="relative z-0">
-				<Contact />
-				<StartsCanvas />
-			</div>
-		</div>
-	</BrowserRouter>
+    <div>
+      Test Site
+    </div>
   )
 }
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
 
 export default App
