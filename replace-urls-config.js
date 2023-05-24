589ef7e48901d0d2bb1cad61ca68b7c8189b38ee
Tonmoy Jitu <52621226+tonmoy0010@users.noreply.github.com>
2023-05-24 18:53:49 +1000

    custom url issue potential fix

---
+++ replace-urls-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    files: 'dist/index.html',
+    from: [
+      /\/personal-site\//g, // Replace "/personal-site/" with an empty string
+    ],
+    to: [
+      '',
+    ],
+  };
+  
