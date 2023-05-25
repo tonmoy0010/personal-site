e90833884efd28cff5dfe7f7018fb89887d61d1b
Tonmoy Ahmed Jitu <tonmoyj@byrons.com.au>
2023-05-25 15:08:18 +1000

    logo issue with mac device --fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,10 +42,11 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+          {/* Header logo for website */}
+          <img src={logo} alt='logo' className='w-13 h-12 object-contain' />
+          {/* <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Tonmoy Jitu
-          </p>
+          </p> */}
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
